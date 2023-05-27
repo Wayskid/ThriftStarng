44788c88d08b6526c91371abc965ce0ff3374200
Wayskid <wayskid02@gmail.com>
2023-05-27 20:01:15 +0100

    Bug fixes

--- Servers/routes/productsRoute.js
+++ Servers/routes/productsRoute.js
@@ -4,68 +4,48 @@ import Product from "../models/productModel.js";
 
 const productsRoute = express.Router();
 
-//Get All Products
-// productsRoute.get(
-//   "/",
-//   asyncHandler(async (req, res) => {
-//     const keyword = req.query.s
-//       ? {
-//           name: {
-//             $regex: req.query.s,
-//             $options: "i",
-//           },
-//         }
-//       : {};
-
-//     const products = await Product.find({
-//       ...keyword,
-//     });
-//     res.json(products);
-//   })
-// );
 // //Get All Products
 productsRoute.get(
   "/",
   asyncHandler(async (req, res) => {
-    // const keyword =
-    //   req.query.s || req.query.c
-    //     ? {
-    //         $or: [
-    //           {
-    //             name: {
-    //               $regex: req.query.s,
-    //               $options: "i",
-    //             },
-    //           },
-    //           {
-    //             category: {
-    //               $regex: req.query.c,
-    //               $options: "i",
-    //             },
-    //           },
-    //         ],
-    //       }
-    //     : {};
+    const keyword = req.query
+      ? {
+          $or: [
+            {
+              name: {
+                $regex: req.query.s,
+                $options: "i",
+              },
+            },
+            {
+              category: {
+                $regex: req.query.c,
+                $options: "i",
+              },
+            },
+          ],
+        }
+      : {};
 
-    const products = await Product.find({
-      $or: [
-        {
-          name: {
-            $regex: req.query.s,
-            $options: "i",
-          },
-        },
-        {
-          category: {
-            $regex: req.query.c,
-            $options: "i",
-          },
-        },
-      ],
-    });
     // const products = await Product.find({
-    //   ...keyword,
+    //   $or: [
+    //     {
+    //       name: {
+    //         $regex: req.query.s,
+    //         $options: "i",
+    //       },
+    //     },
+    //     {
+    //       category: {
+    //         $regex: req.query.c,
+    //         $options: "i",
+    //       },
+    //     },
+    //   ],
     // });
+    const products = await Product.find({
+      ...keyword,
+    });
     // .sort({ [req.query.sort]: req.query.order });
     res.json(products);
   })
