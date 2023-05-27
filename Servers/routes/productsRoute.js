f9c69e7b0050ea59b6e1eaa48745f8ceebe27c53
Wayskid <wayskid02@gmail.com>
2023-05-27 12:39:40 +0100

    Bug fixes

--- Servers/routes/productsRoute.js
+++ Servers/routes/productsRoute.js
@@ -8,32 +8,51 @@ const productsRoute = express.Router();
 productsRoute.get(
   "/",
   asyncHandler(async (req, res) => {
-    const keyword =
-      req.query.s || req.query.c
-        ? {
-            $or: [
-              ({
-                name: {
-                  $regex: req.query.s,
-                  $options: "i",
-                },
-              },
-              {
-                category: {
-                  $regex: req.query.c,
-                  $options: "i",
-                },
-              }),
-            ],
-          }
-        : {};
+    const keyword = req.query.s
+      ? {
+          name: {
+            $regex: req.query.s,
+            $options: "i",
+          },
+        }
+      : {};
 
     const products = await Product.find({
       ...keyword,
-    }).sort({ [req.query.sort]: req.query.order });
+    });
     res.json(products);
   })
 );
+// //Get All Products
+// productsRoute.get(
+//   "/",
+//   asyncHandler(async (req, res) => {
+//     const keyword =
+//       req.query.s || req.query.c
+//         ? {
+//             $or: [
+//               ({
+//                 name: {
+//                   $regex: req.query.s,
+//                   $options: "i",
+//                 },
+//               },
+//               {
+//                 category: {
+//                   $regex: req.query.c,
+//                   $options: "i",
+//                 },
+//               }),
+//             ],
+//           }
+//         : {};
+
+//     const products = await Product.find({
+//       ...keyword,
+//     }).sort({ [req.query.sort]: req.query.order });
+//     res.json(products);
+//   })
+// );
 
 //Get One Product
 productsRoute.get(
